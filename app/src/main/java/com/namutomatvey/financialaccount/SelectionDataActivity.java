0a457846b04a5002c75c443df3d685d3ba12eacf
famiyNataMatvey-AndroidGit <fami1y.NataMatvey@gmail.com>
2018-11-22 22:59:10 +0300

    Создание необходимых страниц, без разметки Part 1

---
+++ app/src/main/java/com/namutomatvey/financialaccount/SelectionDataActivity.java
@@ -0,0 +1,13 @@
+package com.namutomatvey.financialaccount;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SelectionDataActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_selection_data);
+    }
+}
