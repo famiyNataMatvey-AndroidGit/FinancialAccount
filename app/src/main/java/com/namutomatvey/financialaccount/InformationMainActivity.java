b97c868f3c6def44cc6a5907a300546abeb1312b
famiyNataMatvey-AndroidGit <fami1y.NataMatvey@gmail.com>
2018-11-24 14:45:19 +0300

    Добавил Новую Activity. Для отображения значений Доходов, Расходов, Сбережений

---
+++ app/src/main/java/com/namutomatvey/financialaccount/InformationMainActivity.java
@@ -0,0 +1,13 @@
+package com.namutomatvey.financialaccount;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InformationMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_information_main);
+    }
+}
