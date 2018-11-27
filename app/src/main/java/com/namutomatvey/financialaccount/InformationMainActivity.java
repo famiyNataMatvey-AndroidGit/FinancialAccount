0d9baaeb911362c5120decfa9cddd42ce873bc27
famiyNataMatvey-AndroidGit <fami1y.NataMatvey@gmail.com>
2018-11-28 00:13:05 +0300

    Начал писать функции перехода между страницами. Ресурсы.

--- app/src/main/java/com/namutomatvey/financialaccount/InformationMainActivity.java
+++ app/src/main/java/com/namutomatvey/financialaccount/InformationMainActivity.java
@@ -2,12 +2,27 @@ package com.namutomatvey.financialaccount;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class InformationMainActivity extends AppCompatActivity {
+    Integer numberButtonActivityMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_information_main);
+
+        String title = getIntent().getExtras().getString("title",  getResources().getString(R.string.app_name));
+        setTitle(title);
+        numberButtonActivityMain = getIntent().getExtras().getInt("number",  getResources().getInteger(R.integer.error));
+        Button enterButton = findViewById(R.id.buttonEnter);
+        enterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                intent.putExtra("title", expensesTextView.getText().toString());
+//                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/namutomatvey/financialaccount/MainActivity.java
+++ app/src/main/java/com/namutomatvey/financialaccount/MainActivity.java
@@ -1,7 +1,11 @@
 package com.namutomatvey.financialaccount;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,51 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Intent intent = new Intent(MainActivity.this, InformationMainActivity.class);
+
+        // в ключ username пихаем текст из первого текстового поля
+
+        ImageButton expensesImageButton = findViewById(R.id.imageButtonExpenses);
+        ImageButton incomeImageButton = findViewById(R.id.imageButtonIncome);
+        ImageButton moneyboxImageButton = findViewById(R.id.imageButtonMoneybox);
+        ImageButton statisticsImageButton = findViewById(R.id.imageButtonStatistics);
+
+        expensesImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView expensesTextView = findViewById(R.id.textViewExpenses);
+                intent.putExtra("title", expensesTextView.getText().toString());
+                intent.putExtra("number", getResources().getInteger(R.integer.click_button_expenses));
+                startActivity(intent);
+            }
+        });
+        incomeImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView incomeTextView = findViewById(R.id.textViewIncome);
+                intent.putExtra("title", incomeTextView.getText().toString());
+                intent.putExtra("number", getResources().getInteger(R.integer.click_button_income));
+                startActivity(intent);
+            }
+        });
+        moneyboxImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView moneyboxTextView = findViewById(R.id.textViewMoneybox);
+                intent.putExtra("title", moneyboxTextView.getText().toString());
+                intent.putExtra("number", getResources().getInteger(R.integer.click_button_moneybox));
+                startActivity(intent);
+            }
+        });
+        statisticsImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView statisticsTextView = findViewById(R.id.textViewStatistics);
+                intent.putExtra("title", statisticsTextView.getText().toString());
+                intent.putExtra("number", getResources().getInteger(R.integer.click_button_statistics));
+                startActivity(intent);
+            }
+        });
     }
+
 }
