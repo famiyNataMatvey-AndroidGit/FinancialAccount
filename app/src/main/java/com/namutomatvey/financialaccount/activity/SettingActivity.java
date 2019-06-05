8a40486ff5a1d8d2d619d3866c7e711f7488d987
mdovgalev <Come.on.above@gmail.com>
2019-06-06 01:55:07 +0300

    Переход в ФНС через настройки

--- app/src/main/java/com/namutomatvey/financialaccount/activity/SettingActivity.java
+++ app/src/main/java/com/namutomatvey/financialaccount/activity/SettingActivity.java
@@ -1,11 +1,14 @@
 package com.namutomatvey.financialaccount.activity;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.namutomatvey.financialaccount.R;
 
@@ -13,6 +16,7 @@ public class SettingActivity extends AppCompatActivity {
 
     private Toolbar mActionBarToolbar;
     private SharedPreferences mSettings;
+    private Button buttonGoToFns;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +26,17 @@ public class SettingActivity extends AppCompatActivity {
         mActionBarToolbar = findViewById(R.id.toolbar);
         mActionBarToolbar.setTitle(getResources().getString(R.string.title_activity_settings));
         setSupportActionBar(mActionBarToolbar);
+
+        buttonGoToFns = findViewById(R.id.buttonGoToFns);
+        buttonGoToFns.setOnClickListener(
+            new View.OnClickListener() {
+                 @Override
+                 public void onClick(View v) {
+                     Intent intent = new Intent(SettingActivity.this, FnsActivity.class);
+                     startActivity(intent);
+                 }
+             }
+        );
 
     }
 
